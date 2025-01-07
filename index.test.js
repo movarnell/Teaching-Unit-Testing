ed972b1e552232abbbb717ca0db46b14e748a349
movarnell <movarnell@gmail.com>
2025-01-07 10:10:44 -0600

    Add tests for subtract, multiply, and divide functions

--- index.test.js
+++ index.test.js
@@ -5,3 +5,25 @@ describe('add function', () => {
         expect(add(1, "2")).to.not.equal(3);
     });
 });
+
+describe('subtract function', () => {
+    it('should subtract two numbers', () => {
+        expect(subtract(1, 2)).to.equal(-1);
+    });
+});
+
+describe('multiply function', () => {
+    it('should multiply two numbers', () => {
+        expect(multiply(1, 2)).to.equal(2);
+    });
+});
+
+describe('divide function', () => {
+    it('should divide two numbers', () => {
+        expect(divide(1, 2)).to.equal(0.5);
+    });
+
+    it('should throw an error when dividing by zero', () => {
+        expect(() => divide(1, 0)).to.throw("Cannot divide by zero");
+    });
+});
